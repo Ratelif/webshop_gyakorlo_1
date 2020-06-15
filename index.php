c819df4be5a24352a33fe7ae952908583e2ff7f2
Ratelif <csistvan0520@gmail.com>
2020-06-15 23:31:25 +0200

    master_ag github elokeszites

--- index.php
+++ index.php
@@ -16,6 +16,27 @@ include_once "header.php";
 
 <?php
 
+/* https://github.com/Ratelif/webshop_gyakorlo_1.git
+or create a new repository on the command line
+
+echo "# webshop_gyakorlo_1" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Ratelif/webshop_gyakorlo_1.git
+git push -u origin master
+
+or push an existing repository from the command line
+
+git remote add origin https://github.com/Ratelif/webshop_gyakorlo_1.git
+git push -u origin master
+
+or import code from another repository
+
+You can initialize this repository with code from a Subversion, Mercurial, or TFS project. */
+
+
+
 $x = 15;
 for ($i=0; $i < $x; $i++) { 
       echo "<a style='color:red;'>a változó aktuális értéke: ".$i."</a><br>";
